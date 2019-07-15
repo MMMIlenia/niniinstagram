09f0004405d68264b4f9e269877541067cd48861
袁佳妮 <yuanjiani@yuanjianideMacBook-Pro.local>
2019-07-14 23:44:04 -0400

    Initial commit

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
     
 
 
 # Create your views here.
-class HelloDjango(TemplateView)
-    self.template_name = "home.html"
+class HelloDjango(TemplateView):
+    template_name = "home.html"
+
+class PostView(ListView):
+    model = Post
+    template_name = "posts.html"
